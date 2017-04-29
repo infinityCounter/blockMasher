754fda5424d0d6c6b843ad623432c870305308a6
infinityCounter <emilekeith3.0@gmail.com>
2017-04-29 03:52:49 -0500

    Commented code

--- document.js
+++ document.js
@@ -1,12 +1,19 @@
+//When document loaded
 $(document).ready(function(){
 
-    var gameOver = false;
-    const blockWidth = 70, blockHeight = 40;
+    var gameOver = false; //Of no use yet
+    const blockWidth = 70, blockHeight = 40; 
 
+    /**
+     * Generate Random html color code
+     * 
+     * */
     function randomColor(){
         var colorCode = "#";
         while(true){
+            //Generate random number between 0 and 15
             var randNum = Math.floor(Math.random() * 16);
+            //Convert to hexadecimal char, 0 - F and append to colorCode string
             colorCode +=  randNum.toString(16);
             if(colorCode.length >= 7)
                 break;
@@ -15,11 +22,21 @@ $(document).ready(function(){
     }
 
 
+    /**
+     * Generate blocks 
+     * 
+     * int numOfBlocks
+     * 2dContext context
+     */
     function generateBlocks(numOfBlocks, context){
 
+        //Start at the top left
         var posFromTop = posFromLeft = 20;
+        //Array to hold position of blocks for redrawing
         var blockConfigArray = [];
         for(var counter = 0; counter < numOfBlocks; counter++ ){
+
+            //Draw block
             context.beginPath();
             context.rect(posFromLeft, posFromTop, blockWidth, blockHeight);
             color = randomColor();
@@ -35,6 +52,7 @@ $(document).ready(function(){
 
             blockConfigArray.push(blockConfig);
 
+            //Shift position to draw next block
             posFromLeft += 90;
             if(posFromLeft > 420){
                 posFromLeft = 20;
@@ -44,6 +62,16 @@ $(document).ready(function(){
         return blockConfigArray;
     }
 
+    /**
+     * Generate launch bar
+     * 
+     * 2dContext context
+     * int canvasWidth
+     * int canvasHeight
+     * 
+     * Geenrates launch bar centered in the lower canvas.
+     * Returns a function closure to move the bar (redraw in new position)
+     */
     function generateBar(context, canvasWidth, canvasHeight){
 
         const barWidth = 90;
@@ -51,26 +79,41 @@ $(document).ready(function(){
         const posFromTop = canvasHeight - 40;
         var posFromLeft = (canvasWidth - barWidth)/2;
         const color = randomColor();
+        //Draw bar
         context.beginPath();
         context.rect(posFromLeft, posFromTop, barWidth, barHeight);
         context.fillStyle = color;
         context.fill();
         context.closePath();
 
+        /**
+         * Move bar left or right
+         * 
+         * int lor
+         * 
+         * Accepts an integer, and moves bar in direction. 0 for left, any non zero number for right
+         */
         return function(lor){
+
             context.beginPath();
+            //If to move left
             if(lor === 0){
+                //draw 10 pixels to the left
                 posFromLeft -= 10;
                 if(posFromLeft < 0) {
                     posFromLeft = 0;
                 }
             }else{
                 posFromLeft += 10;
+                /*If position from left is greater than or equal to canvas width minus bar width
+                * i.e. If the bar has touched the edge of the canvas
+                */
                 if(posFromLeft >= canvasWidth - barWidth) {
                     posFromLeft = canvasWidth - barWidth;
                 }
 
             }
+            //draw bar
             context.rect(posFromLeft, posFromTop, barWidth, barHeight);
             context.fillStyle = color;
             context.fill();
@@ -78,6 +121,14 @@ $(document).ready(function(){
         }
     }
 
+    /**
+     * Redraw Canvas after clearing
+     * 
+     * Array blockConfigArr
+     * 2dContext context
+     * 
+     * Draws onto context from data in blockConfigArr
+     */
     function redrawCanvas(blockConfigArr, context){
 
         blockConfigArr.forEach(function(blockConfig) {
@@ -89,31 +140,41 @@ $(document).ready(function(){
         }, this);
     }
     
+    /**
+     * Main Function
+     * 
+     */
     function main(){
 
         const canvas = $("#gameArea")[0];
         var canvasContext = canvas.getContext('2d');
+        //Generate blocks
         var blockConfigs = generateBlocks(10, canvasContext);
+        //Generate bar
         const bar = generateBar(canvasContext, canvas.width, canvas.height);
 
+        //Add even listener to the page
         $(document).keydown(function(e){
             e.preventDefault();
-            console.log("keydown");
-            console.log(e);
-            console.log(canvas.height);
-            console.log(canvas.width);
+            //If left arrow key pressed
             if(e.keyCode == 37){
+                //Clear canvas
                 canvasContext.clearRect(0, 0, canvas.width, canvas.height);
                 redrawCanvas(blockConfigs, canvasContext);
+                //Move launch bar left
                 bar(0);
+
             }else if(e.keyCode == 39){
+                //If right arrow pressed
                 canvasContext.clearRect(0, 0, canvas.width, canvas.height);
                 redrawCanvas(blockConfigs, canvasContext);
+                //Move launch bar right
                 bar(1);
             }
         });
 
     }
 
+    //Execute program
     main();
 });
